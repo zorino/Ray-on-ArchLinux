6afb590e7627539ee7515828e02ce546e8b8ebc9
Sebastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-11-05 03:16:56 +0000

    The new PKGBUILD passes tests !
    Signed-off-by: Sebastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- QualityControl.sh
+++ QualityControl.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 run(){
+	echo "TestForArchLinux"
 	command=$1
 	echo "$(date)) $command"
 	$command
 	echo ""
 }
 
+(
 run "namcap PKGBUILD"
 run "makepkg -f PKGBUILD"
 run "ls *.xz"
 run "namcap *.xz"
 
-
+) &> QualityControl-$(date +%Y-%m-%d-%H-%M-%S-%s).txt
